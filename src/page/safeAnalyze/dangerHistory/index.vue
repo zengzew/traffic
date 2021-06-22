714a8d8d74b71b4cc9bf396b626acf1eae171b05
zengzew <2625533610@qq.com>
2021-06-22 15:11:13 +0800

    一些优化

--- src/page/safeAnalyze/dangerHistory/index.vue
+++ src/page/safeAnalyze/dangerHistory/index.vue
@@ -6,9 +6,7 @@
       <el-tab-pane label="历史情况" name="first"
         ><history-situation></history-situation
       ></el-tab-pane>
-      <el-tab-pane label="历史点时间" name="second"
-        ><history-events></history-events
-      ></el-tab-pane>
+      <el-tab-pane label="历史点时间" name="second"></el-tab-pane>
     </el-tabs>
   </div>
 </template>
